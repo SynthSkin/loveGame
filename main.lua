c8e77c20f26f1f544a5f6aca74eaba03a0c2c33a
SynthSkin <sh93858@tcc.edu>
2026-09-22 13:47:15 -0400

    git ignore start script

--- main.lua
+++ main.lua
@@ -1,11 +1,5 @@
 local InputManager = require("Handlers.Input").new()
 
-local Listener = InputManager:CreateListener("k", "start", nil)
-
-Listener.Signal:Connect(function ()
-	print("k input recieved")
-end)
-
 function love.keypressed(k)
     InputManager:TriggerInput(k, "start")
 end
